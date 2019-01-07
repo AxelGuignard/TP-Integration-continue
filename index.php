475d010e528ed60b258faaf67690c56a12d30cfd
Axel Guignard <axelguignard@yahoo.fr>
2019-01-07 16:16:15 +0100

    Added templates Added basic empty index

--- index.php
+++ index.php
@@ -5,4 +5,22 @@
  * Date: 07/01/2019
  * Time: 16:08
  */
+?>
 
+<!DOCTYPE HTML>
+<html>
+<?php
+include 'template/head.php';
+?>
+<body>
+    <?php
+    include 'template/header.php';
+    ?>
+
+
+
+    <?php
+    include 'template/footer.php';
+    ?>
+</body>
+</html>
---
+++ template/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: axelg
+ * Date: 07/01/2019
+ * Time: 16:13
+ */
